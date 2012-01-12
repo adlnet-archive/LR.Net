ac93d5a4ab2cca87ddfac700427e10b5c0ce2cc6
Austin Montoya <austin.montoya.ctr@adlnet.gov>
2012-01-12 14:57:20 -0500

    added slice result

---
+++ LearningRegistry/SliceResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LearningRegistry
+{
+    public class SliceResult : ResumableResult
+    {
+        protected override ResumableResult getPage()
+        {
+            return LRUtils.Slice(this.BaseUri, this._Args);
+        }
+    }
+}
